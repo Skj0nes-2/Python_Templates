f112bf2d397fd7faf8247775240cfb1c2979b16f
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-03-19 13:47:43 -0400

    Create Classic.py

---
+++ Templates/Classic.py
@@ -0,0 +1,38 @@
+# Notes:
+#    All sections are optional!
+#    Classic is often used for simple operations such as a calculator
+
+# Language Definitions
+#    1. Persistent Memory; refers to a file at the main file's location containing data
+#    2. File; refers to a file at the location that the user is operating from containg data
+#    3. Temporary Memory; refers to data stored temporarily, in the main file, while the main file is running
+
+# Pros & Cons:
+
+#   Pros:
+#       1. Is easier to read / write as a developer
+
+#   Cons:
+#       1. Can be harder to reuse / repeat sections of the code
+
+# Uses:
+#    1. Simple Operations; Reasons: Simpler, easy functions
+
+# All below is the template
+
+
+# Imports - Import
+
+# Definitions - Establish definitions / Import definitions
+
+# Input - Take input from user
+
+# Variables - Establish variables
+
+# Load - Load persistent memory / Load file
+
+# Main - Logic / Modify temporary memory / Modify persistent memory / Modify file
+
+# Output - Show Result / Write result to file / Write result to persistent memory
+
+# Save - Save persistent memory / Write to file
