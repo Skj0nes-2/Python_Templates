7cd9d34d1122dccfc8061a995ad741c0a355cdcc
Aric Jones <aric.jones@student.msdmartinsville.org>
2025-03-19 13:48:13 -0400

    Create RunLoop.py

---
+++ Templates/RunLoop.py
@@ -0,0 +1,57 @@
+# Notes:
+#    All sections are optional!
+#    Run Loop is often used in more complicted projects such as games
+#    Code does not have to loop!
+#    Not all code has to be contained within a function!
+
+# Language Definitions:
+#    1. Persistent Memory; refers to a file at the main file's location containing data
+#    2. File; refers to a file at any location containg data
+#    3. Temporary Memory; refers to data stored temporarily, in the main file, while the main file is running; Examples: Variable, Function, Etc
+#    4. Run Loop; When code is seperated into functions and then the functions are run
+
+# Pros & Cons:
+
+#   Pros:
+#       1. Allows for easy reuse / repeating of sections of the code
+
+#   Cons:
+#       1. Can be harder to read / write as a developer
+
+# Uses:
+#    1. Games; Reasons: More robust functions
+#    2. Complicated operations; Reasons: More robust functions
+
+# All below is the template
+
+
+# Imports
+
+# Definitions - Establish definitions / Import definitions
+
+#    Extra Definitions  - Definitions not part of run loop
+    
+#    Input Definition  - Take input from user
+    
+#    Load Definition - Load persistent memory / Load file
+    
+#    Main Definition - Logic / Modify temporary memory / Modify persistent memory / Modify file
+    
+#    Output Definition - Show Result / Write result to file / Write result to persistent memory
+    
+#    Save Definition - Save persistent memory / Write to file
+    
+
+# Variables
+
+# Run Loop - Runs definitions containing code and general code
+
+#    Run Input
+    
+#    Run Load
+    
+#    Run Main
+    
+#    Run Output
+    
+#    Run Save
